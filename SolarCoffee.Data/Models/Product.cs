7d7207390e9c2cd8efc087496924227d43c2f801
chris-pasquali <47729570+chris-pasquali@users.noreply.github.com>
2020-07-22 12:13:46 +1000

    Adding more models

---
+++ SolarCoffee.Data/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SolarCoffee.Data.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        [MaxLength(70)]
+        public string Name { get; set; }
+        [MaxLength(150)]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsTaxable { get; set; }
+        public bool IsArchived { get; set; }
+    }
+}
---
+++ SolarCoffee.Data/SolarDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SolarCoffee.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarCoffee.Data
+{
+    public class SolarDBContext : IdentityDbContext
+    {
+        public SolarDBContext() { }
+        public SolarDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<CustomerAddress> CustomerAddresses { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductInventory> ProductInventories { get; set; }
+        public virtual DbSet<ProductInventorySnapshot> ProductInventorySnapshots { get; set; }
+        public virtual DbSet<SalesOrder> SalesOrders { get; set; }
+        public virtual DbSet<SalesOrderItem> SalesOrderItems { get; set; }
+
+
+    }
+}
