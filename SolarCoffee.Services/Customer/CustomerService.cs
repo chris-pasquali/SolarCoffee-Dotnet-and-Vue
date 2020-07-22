3ffd5db14c67bb1000799848172d2231e038a6b0
chris-pasquali <47729570+chris-pasquali@users.noreply.github.com>
2020-07-22 23:23:13 +1000

    Add implementation and interfaces in service layer

---
+++ SolarCoffee.Services/Customer/CustomerService.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolarCoffee.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SolarCoffee.Services.Customer
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly SolarDBContext _db;
+
+        public CustomerService(SolarDBContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        /// <summary>
+        /// Adds a new Customer record
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns>ServiceResponse<Customer></Customer></returns>
+        public ServiceResponse<Data.Models.Customer> CreateCustomer(Data.Models.Customer customer)
+        {
+            try
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+                return new ServiceResponse<Data.Models.Customer>
+                {
+                    IsSuccess = true,
+                    Message = "New Customer added",
+                    Time = DateTime.UtcNow,
+                    Data = customer
+                };
+            }
+            catch (Exception e)
+            {
+                return new ServiceResponse<Data.Models.Customer>
+                {
+                    IsSuccess = false,
+                    Message = e.StackTrace,
+                    Time = DateTime.UtcNow,
+                    Data = customer
+                };
+            }
+        }
+
+        /// <summary>
+        /// Deletes a customer record
+        /// </summary>
+        /// <param name="id">int customer primary key</param>
+        /// <returns>ServiceResponse<bool></bool></returns>
+        public ServiceResponse<bool> DeleteCustomer(int id)
+        {
+            var customer = _db.Customers.Find(id);
+            var now = DateTime.Now;
+
+            if (customer == null)
+            {
+                return new ServiceResponse<bool>
+                {
+                    Time = now,
+                    IsSuccess = false,
+                    Message = "Customer to delete not found",
+                    Data = false
+                };
+            }
+
+            try
+            {
+                _db.Customers.Remove(customer);
+                _db.SaveChanges();
+
+                return new ServiceResponse<bool>
+                {
+                    Time = now,
+                    IsSuccess = true,
+                    Message = "Customer deleted",
+                    Data = true
+                };
+            }
+            catch (Exception e)
+            {
+                return new ServiceResponse<bool>
+                {
+                    Time = now,
+                    IsSuccess = false,
+                    Message = e.StackTrace,
+                    Data = false
+                };
+            }
+        }
+        
+        /// <summary>
+        /// Returns a list of Customers from the database with thier CustomerAddress
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns>List<Customer></Customer></returns>
+        public List<Data.Models.Customer> GetAllCustomers()
+        {
+            return _db.Customers
+                .Include(customer => customer.PrimaryAddress)
+                .OrderBy(customer => customer.LastName)
+                .ToList();
+        }
+
+        /// <summary>
+        /// Gets a Customer record by primary key
+        /// </summary>
+        /// <param name="id">int customer primary key</param>
+        /// <returns>Customer</returns>
+        public Data.Models.Customer GetById(int id)
+        {
+            return _db.Customers.Find(id);
+        }
+    }
+}
